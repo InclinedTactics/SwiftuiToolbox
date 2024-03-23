4e153fa49aa0d488eb211682b1ee4b5c2a7834bd
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-03-23 15:42:11 -0500

    Initial Commit

---
+++ SwiftuiToolbox/SwiftuiToolboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiToolboxApp.swift
+//  SwiftuiToolbox
+//
+//  Created by J. DeWeese on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiToolboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
